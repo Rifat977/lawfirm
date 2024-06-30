3bf46819ecd8dc7080219ee155de5fdcdc523e39
rifat977 <itscrifat5147@gmail.com>
2024-06-30 23:05:07 +0600

    fix design and issues

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,9 @@
-from .models import SiteSettings
+from .models import SiteSettings, AboutUs
 
 def site_settings(request):
     settings = SiteSettings.objects.first()
     return {'site_settings': settings}
+
+def site_about(request):
+    site_about = AboutUs.objects.first()
+    return {'site_about': site_about}
